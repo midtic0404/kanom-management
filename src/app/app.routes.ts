a985dfd9e8c5d77981680b685f21aa9447cb8fe2
Cha Cha <midtic0404@gmail.com>
2023-05-17 23:12:05 -0600

    Added routing for dashboard and recipes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: DashboardComponent, pathMatch: 'full'},
+    {path: 'recipes', component: RecipesComponent},
+    // {path: '', redirectTo: '/recipes', pathMatch: 'full'}
+];
